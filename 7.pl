711971baaf40b621bbbb8d8591d8df7aee4aaaa6
Jos Boumans <jos@dwim.org>
2015-08-29 16:49:00 -0700

    * solution for 49

--- 7.pl
+++ 7.pl
@@ -1,7 +1,7 @@
 # http://projecteuler.net/index.php?section=problems&id=7
-# 
+#
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-# 
+#
 # What is the 10001st prime number?
 
 use strict;
@@ -14,14 +14,14 @@ my @Primes;                     # pre-computed list holding all primes
 my $i = 1;
 my $found;
 while ($i++) {
-    
-    my $flag = 1;
-    for my $p ( @Primes ) {  
-        $i % $p || ( $flag = 0, last ); # undevidable, not a prime
-    }             
 
-    do { $found++; push @Primes, $i } if $flag;    
-    
+    my $flag = 1;
+    for my $p ( @Primes ) {
+        $i % $p || ( $flag = 0, last ); # undevidable, not a prime
+    }
+
+    do { $found++; push @Primes, $i } if $flag;
+
     last if $found == 10001;
 }
 
